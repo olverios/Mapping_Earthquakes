30723ceef246fdfc5181fb4c728beed44143c519
Olverios <91705406+olverios@users.noreply.github.com>
2022-06-13 15:41:21 -0500

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,12 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
+ let map= L.map('mapid').setView([43.0522, - 118.2437],14)
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+let marker = L.circle([34.0522, -118.2437], {
+    radius: 100
+ }).addTo(map);
 // // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
 //     center: [
@@ -10,6 +14,7 @@ let marker = L.marker([34.0522, -118.2437]).addTo(map);
 //     ],
 //     zoom: 4
 //   });
+
 // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {

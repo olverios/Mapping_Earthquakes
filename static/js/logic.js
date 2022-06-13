931d1c6005e40d37bb730bb298c3fbe5bd287797
Olverios <91705406+olverios@users.noreply.github.com>
2022-06-13 18:15:15 -0500

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,9 +1,21 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
- let map= L.map('mapid').setView([43.0522, - 118.2437],14)
+ let map= L.map('mapid').setView([36.1733, -120.1794],7)
+ // Coordinates for each point to be used in the line.
+ let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
 // Get data from cities.js
-let cityData = cities;
+let cityData = cities; 
 
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
@@ -38,7 +50,7 @@ cityData.forEach(function(city) {
 
 // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     // mapbox/streets-v11
     // mapbox/outdoors-v11
